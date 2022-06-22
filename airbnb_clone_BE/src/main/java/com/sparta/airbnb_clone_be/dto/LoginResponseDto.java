87cfc76cf479eabcc5a54f4d3fa5f63218c3ab7a
bapunn <kokosunsun1004@Gmail.com>
2022-06-22 12:19:41 +0900

    Revert "tempSave : kakao 로그인 중간저장"
    This reverts commit 18f3e266e027463ff0a53e73c889a6398e622332.

--- airbnb_clone_BE/src/main/java/com/sparta/airbnb_clone_be/dto/LoginResponseDto.java
+++ airbnb_clone_BE/src/main/java/com/sparta/airbnb_clone_be/dto/LoginResponseDto.java
@@ -9,11 +9,11 @@ public class LoginResponseDto {
 
     private boolean is_login;
 
-    private String token;
+    private String accessToken;
 
-    public LoginResponseDto(String nickname, boolean is_login, String token) {
+    public LoginResponseDto(String nickname, boolean is_login, String accessToken) {
         this.nickname = nickname;
         this.is_login = is_login;
-        this.token = token;
+        this.accessToken = accessToken;
     }
 }
