8a1d29e15a5e6aaf23a3e19de5622fb2be16b2eb
Son-Gyeongi <thsruddl77@gmail.com>
2022-06-22 21:13:32 +0900

    edit : 댓글 시간형태 변경, 별점 평균 계산 변경

--- airbnb_clone_BE/src/main/java/com/sparta/airbnb_clone_be/dto/response/CommentResponseDto.java
+++ airbnb_clone_BE/src/main/java/com/sparta/airbnb_clone_be/dto/response/CommentResponseDto.java
@@ -1,10 +1,12 @@
 package com.sparta.airbnb_clone_be.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +23,8 @@ public class CommentResponseDto {
 
     private Long accommodation_id;
 
+    @DateTimeFormat(pattern = "yyyy년 MM월")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
 }
